159551907e8867862e53e39150e13c2804231d08
Bill Hsu <bl33234679@gmail.com>
2020-01-13 14:15:11 +0800

    Fix tailing comma and new line tab

--- generator/generate.go
+++ generator/generate.go
@@ -50,8 +50,8 @@ func Generate(topic string, minLen int) string {
 	var ret string
 	for utf8.RuneCountInString(ret) < minLen {
 		x := rand.Intn(100)
-		if x < 5 && utf8.RuneCountInString(ret) != 0 {
-			ret += ".<br>    "
+		if x < 5 && utf8.RuneCountInString(ret) != 0 && ret[len(ret)-1:] == "." {
+			ret += "<br><br>&nbsp;&nbsp;"
 		} else if x < 20 {
 			if len(shuffledFamous) == 0 {
 				break
