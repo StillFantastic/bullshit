108e24ff21e08b5d66de754e22fa4ce15d3a7f7f
Bill Hsu <bl33234679@gmail.com>
2020-07-24 13:11:32 +0800

    Add ip rate limiter

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/StillFantastic/bullshit
+
+go 1.14
+
+require (
+	github.com/rs/cors v1.7.0
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
+)
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"bullshit/generator"
 	"encoding/json"
 	"fmt"
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/StillFantastic/bullshit/generator"
 	"github.com/rs/cors"
+	"golang.org/x/time/rate"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
+	"sync"
 	"time"
 )
 
@@ -18,7 +20,19 @@ type Data struct {
 	MinLen int
 }
 
-func log(topic string, minLen int) {
+type visitor struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
+var visitors = make(map[string]*visitor)
+var mu sync.Mutex
+
+func init() {
+	go cleanupVisitors()
+}
+
+func logRequest(topic string, minLen int) {
 	f, err := os.OpenFile("bullshit_log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
@@ -31,31 +45,61 @@ func log(topic string, minLen int) {
 	}
 }
 
+func getVisitor(ip string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
 
-func ipIsBanned(ip string) bool {
-	bannedIp := os.Getenv("BANNED_IP")
-	ipList := strings.Split(bannedIp, ",")
-	for _, v := range ipList {
-		if strings.Count(ip, v) > 0 {
-			return true
-		}
+	v, exists := visitors[ip]
+	if !exists {
+		limiter := rate.NewLimiter(1, 1000)
+		visitors[ip] = &visitor{limiter, time.Now()}
+		return limiter
 	}
-	return false
+
+	v.lastSeen = time.Now()
+	return v.limiter
 }
 
+func cleanupVisitors() {
+	for {
+		time.Sleep(time.Minute)
+
+		mu.Lock()
+		for ip, v := range visitors {
+			if time.Since(v.lastSeen) > 20*time.Minute {
+				delete(visitors, ip)
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+		}
+
+		limiter := getVisitor(ip)
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 func bullshitHandler(w http.ResponseWriter, r *http.Request) {
 	var d Data
 	err := json.NewDecoder(r.Body).Decode(&d)
 	if err != nil {
-		fmt.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
 	}
 
-	if ipIsBanned(r.RemoteAddr) {
-		return
-	}
-
-	log(d.Topic, d.MinLen)
+	logRequest(d.Topic, d.MinLen)
 	ret := generator.Generate(d.Topic, d.MinLen)
 	w.Write([]byte(ret))
 }
@@ -70,7 +114,7 @@ func main() {
 	} else {
 		addr = "0.0.0.0:" + os.Args[1]
 	}
-	err := http.ListenAndServe(addr, handler)
+	err := http.ListenAndServe(addr, limit(handler))
 	if err != nil {
 		fmt.Println(err)
 	}
